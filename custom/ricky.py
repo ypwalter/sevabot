b4a0495e35f2f7b07909b9d9ad38a1ea890a9023
Jonathan Hao <johnathan79717@gmail.com>
2015-01-16 20:29:34 +0800

    catch NameError and fix ranking algorithm

--- custom/ricky.py
+++ custom/ricky.py
@@ -4,13 +4,21 @@ import urllib2
 
 try:
     data = json.load(urllib2.urlopen("https://github.com/mozilla-b2g/gaia/graphs/contributors-data"))
-except ValueError as e:
+except (ValueError, NameError) as e:
     print 'Request timeout. Please try again.'
 
-ordinal = ['th', 'st', 'nd', 'rd'] + ['th'] * 6 
+ordinal = ['th', 'st', 'nd', 'rd'] + ['th'] * 6
+commits = 0
 for i in xrange(100):
     if data[i]['author']['login'] == 'RickyChien':
-        rank = 100 - i
+        commits = data[i]['total']
+        m = 100
+        M = 0
+        for j in xrange(100):
+            if data[j]['total'] == commits:
+                m = min(m, j)
+                M = max(M, j)
+        rank = 100 - (m + M - i)
         print 'RickyChien is currently ranked %d%s among all the Gaia contributors in the world.' % (rank, ordinal[rank % 10])
         break
 else:
