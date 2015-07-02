0ab45444dd7637a1dd946336f4aa82eb8ecf1cca
Walter Chen <YPWALTER@WalterdeMacBook-Pro.local>
2015-07-02 17:37:50 +0800

    clean up again

--- custom/gong.py
+++ custom/gong.py
@@ -10,12 +10,12 @@ def main(args):
        print "不要像「小孩子要糖果」似的，什麼都喊要，而且越多越好。一下子得不到想要的也不要疑神疑鬼。"
        return 0
 
-    if args.__len__() == 0:
-       print ""
-       return 0
-
     # magic word
-    magic_word = args[random.randint(0, args.__len__() - 1)]   
+    if args.__len__() == 0:
+       magic_word = args[random.randint(0, args.__len__() - 1)]   
+    else:
+       magic_word = ""
+
     if magic_word == "sf" or magic_word == "paris":
         print "巴黎有華貴的內飾，舊金山有海景，是一種比法；"
     elif magic_word == "tp" or magic_word == "bj":
